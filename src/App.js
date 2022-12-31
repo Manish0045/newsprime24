d667c3c9d3e5eda21ae0968a74ce80f4279cbea6
[Manish Sahani] <[manishsahani345@gmail.com]>
2022-12-31 16:23:12 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ export default class App extends Component {
         <Router>
           <Navbar />
           <Switch>
-            <Route path="/general"><News pageSize={6} country="in" category="general" /></Route>
-            <Route path="/buisiness"><News pageSize={6} country="in" category="buisiness" /></Route>
-            <Route path="/entertainment"><News pageSize={6} country="in" category="entertainment" /></Route>
-            <Route path="/health"><News pageSize={6} country="in" category="health" /></Route>
-            <Route path="/science"><News pageSize={6} country="in" category="science" /></Route>
-            <Route path="/sports"><News pageSize={6} country="in" category="sports" /></Route>
-            <Route path="/technology"><News pageSize={6} country="in" category="technology" /></Route>
+            <Route exact path="/"><News key="general" pageSize={6} country="in" category="general" /></Route>
+            <Route exact path="/buisiness"><News key="buisiness" pageSize={6} country="in" category="buisiness" /></Route>
+            <Route exact path="/entertainment"><News key="entertainment" pageSize={6} country="in" category="entertainment" /></Route>
+            <Route exact path="/health"><News key="health" pageSize={6} country="in" category="health" /></Route>
+            <Route exact path="/science"><News key="science" pageSize={6} country="in" category="science" /></Route>
+            <Route exact path="/sports"><News key="sports" pageSize={6} country="in" category="sports" /></Route>
+            <Route exact path="/technology"><News key="technology" pageSize={6} country="in" category="technology" /></Route>
           </Switch>
         </Router>
       </div>
