9a43e2b086dd41e38998c0814c86bad7668c3232
[Manish Sahani] <[manishsahani345@gmail.com]>
2023-01-04 12:47:07 +0530

    news content

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
 const App = ()=> {
-  const pageSize = 5;
+  const pageSize = 12;
   const apiKey = process.env.REACT_APP_NEWS_API
   const [progress, setProgress] = useState(0)
  
